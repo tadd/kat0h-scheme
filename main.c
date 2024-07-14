09304161e4821f08b8ce37a57b04af9eb5c14386
Tadashi Saito <tad.a.digger@gmail.com>
2024-07-14 21:53:25 +0900

    Make buffer static.

--- main.c
+++ main.c
@@ -784,7 +784,7 @@ frame *mk_initial_env() {
 }
 void repl() {
   puts("kat0h's scheme");
-  char buf[1024];
+  static char buf[1024];
   frame *environ = mk_initial_env();
   printf("scm> ");
   while (fgets(buf, 1024, stdin) != NULL) {
