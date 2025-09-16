tadd/kat0h-my-clisp

Introduce ubsan/asan.

Makefile
@@ -1,5 +1,6 @@
 CC=clang
 CFLAGS=-Og -ggdb3 -fno-omit-frame-pointer $(XCFLAGS)
+ubsan_flags=-fsanitize=undefined,address -fomit-frame-pointer -O2
 OBJS=main.o
 
 all: lisp
@@ -7,15 +8,22 @@ all: lisp
 %.o: %.c
 	$(CC) $(CFLAGS) -c $<
 
+%.ubsan.o: %.c
+	$(CC) $(CFLAGS) $(ubsan_flags) -c $< -o $@
+
 lisp: $(OBJS)
 	$(CC) $(CFLAGS) $^ -o $@
 
+ubsan: lisp-ubsan
+lisp-ubsan: $(OBJS:.o=.ubsan.o)
+	$(CC) $(CFLAGS) $(ubsan_flags) $^ -o $@
+
 clean:
-	rm -f *.o lisp
+	rm -f *.o lisp lisp-*
 
 %.analyze: %.c
 	$(CC) $(CFLAGS) --analyze -c $< -o /dev/null
 
 analyze: $(OBJS:.o=.analyze)
 
-.PHONY: clean analyze
+.PHONY: clean analyze ubsan

tadd/kat0h-scheme

Makefile
@@ -1,5 +1,6 @@
 CC = gcc
 CFLAGS = -Og -ggdb3 -fno-omit-frame-pointer $(XCFLAGS)
+ubsan_flags = -fsanitize=undefined,address -fomit-frame-pointer -O2
 OBJS = main.o continuation.o
 
 all: scm
@@ -10,11 +11,18 @@ scm: $(OBJS)
 %.o: %.c
 	$(CC) $(CFLAGS) -c $<
 
-main.o: main.h
-continuation.o: continuation.h
+%.ubsan.o: %.c
+	$(CC) $(CFLAGS) $(ubsan_flags) -c $< -o $@
+
+main.*o: main.h
+continuation.*o: continuation.h
+
+ubsan: scm-ubsan
+scm-ubsan: $(OBJS:.o=.ubsan.o)
+	$(CC) $(CFLAGS) $(ubsan_flags) $^ -o $@
 
 clean:
-	rm -f *.o scm
+	rm -f *.o scm scm-*
 
 %.analyze: %.c
 	$(CC) $(CFLAGS) --analyzer -c $< -o /dev/null
@@ -24,4 +32,4 @@ analyze: $(OBJS:.o=.analyze)
 tags:
 	ctags -R .
 
-.PHONY: clean analyze
+.PHONY: clean analyze ubsan