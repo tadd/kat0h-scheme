tadd/kat0h-my-clisp

Avoid K&R-style decl.

main.c
@@ -341,7 +341,7 @@ expr *parse_hash_literal() {
     throw("parse error: unexpected token %c", *input);
   }
 }
-expr *parse_list();
+expr *parse_list(void);
 expr *parse_expr() {
 #ifdef DEBUG
   printf("parse_expr: %s\n", input);

tadd/kat0h-scheme

main.c
@@ -240,7 +240,7 @@ value *parse_hash_literal() {
     throw("parse error: unexpected token %c", *input);
   }
 }
-value *parse_list();
+value *parse_list(void);
 value *parse_value() {
 #ifdef DEBUG
   printf("parse_value: %s\n", input);